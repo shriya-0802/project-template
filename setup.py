7ff236f093973acbf1824bf1ebd90884c7f3aeb4
shriya-0802 <shriya.mohanty2004@gmail.com>
2025-02-12 18:29:25 +0530

    project structure updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Chatbot",
+    version="0.0.1",
+    author="shriya",
+    author_email="shriya.mohanty2022@vitstudent.ac.in",
+    packages=find_packages(),
+    install_requires=["langchain_core"]
+)
